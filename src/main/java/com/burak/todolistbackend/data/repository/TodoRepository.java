9f912f52e07e323b391955d51ff9e2d966d1c19e
Burak Kahraman <burakhasankahraman@gmail.com>
2024-05-19 23:05:59 +0300

    Backend Project

--- src/main/java/com/burak/todolistbackend/data/repository/TodoRepository.java
+++ src/main/java/com/burak/todolistbackend/data/repository/TodoRepository.java
@@ -1,11 +1,10 @@
 package com.burak.todolistbackend.data.repository;
 
 import com.burak.todolistbackend.data.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+
 
 @Repository
 public interface TodoRepository extends CrudRepository<Todo,Long> {
